9c03acbf80a60a7fc0c88e42c67be3e9863c50e4
37994 <379949419@qq.com>
2019-11-03 10:46:27 +0800

    V1.0.0 alpha

--- src/main/MainThread.java
+++ src/main/MainThread.java
@@ -10,21 +10,18 @@ import java.net.Socket;
 
 //主线程
 public class MainThread {
-
-
-
     public static void main(String[] args) throws IOException {
 
         String port = (String) new XMLUtil().getBean("serverport");
         //开始接受客户端
-        while(true){
+        while (true) {
             try {
                 Thread.sleep(1000);
                 //如服务器异常关闭，会在这里重启
                 Socket socket = Server.getServer(port).accept();
                 User u = new User("windy@qq.com", "123456", socket);
                 Channel channel = new Channel(u);
-                System.out.println(channel+"已经连接");
+                System.out.println(channel + "已经连接");
                 Server.channels.add(channel);
                 new Thread(channel).start();
             } catch (Exception e) {
